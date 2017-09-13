ad6813bc0c7b96be07342f0da74efa15f6c18de1
Dan Gilleland <dgilleland@nait.ca>
2017-09-13 14:26:22 -0600

    OOP Sample - Person w. Fields

---
+++ Intro to OOP Basics/OOPBasics/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPBasics
+{
+    // This is a very simple class with two "fields" that hold information.
+    // When I declare a class, I'm designing a new "data type".
+    public class Person
+    {
+        // Declare a couple of "fields" to hold information
+        public string Name;
+        public int Age;
+    }
+}
---
+++ Intro to OOP Basics/OOPBasics/Program.cs
@@ -0,0 +1,51 @@
+﻿/* File: Program.cs
+ * Author: Dan Gilleland
+ * Purpose: Drivers to demonstrate various OOP Examples
+ */
+using System;
+
+namespace OOPBasics
+{
+    public class Program
+    {     
+        public static void Main(string[] args) // method header
+        {   // Open curly brace is the start of the body
+            Console.WriteLine("Hello OOP Basics!");
+
+            string name; // Declaring a variable
+            // Prompt - asking the user to enter some information
+            Console.Write("What is your name? ");
+            // Read the user's input
+            name = Console.ReadLine(); // Capture the user's input
+
+            // String interpolation allows me to inject my variable
+            // values directly inside a string.
+            Console.WriteLine($"Hello {name}!");
+
+            Console.WriteLine("Good to meet you {0}", name);
+
+            // Demonstrate using the Person data type (class)
+            // to create variables (objects)
+            Person me;
+            Person myFriend;
+            // Instantiate (create new) Person objects
+            me = new Person(); // new is a keyword
+            // Assign what my name/age is
+            me.Name = "Dan Gilleland";
+            me.Age = 40; // that's a lie....
+
+            // Instantiate another Person object
+            myFriend = new Person()
+            { // initialization list..
+                Name = "Fred Flintstone",
+                Age = 43
+            };
+
+
+            string message;
+            message = $"My name is {me.Name} and my friend is {myFriend.Name}";
+            Console.WriteLine(message);
+
+        }   // end of Main() method body
+    }
+}
