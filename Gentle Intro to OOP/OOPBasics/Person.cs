17dcfc0c55130ffaa86db81364d14b0d83a41ff8
Dan Gilleland <dgilleland@nait.ca>
2017-09-15 08:49:56 -0600

    OOP Basics - Gentle Intro

---
+++ Gentle Intro to OOP/OOPBasics/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPBasics
+{
+    public class Person
+    {
+        // Fields - member variable to hold information
+
+        // Properties - look like fields but internally act like methods
+        //            - Properties provide controlled access to data
+
+        // Constructors - ensure fields/properties have "meaningful" data
+
+        // Methods - sets of instructions; represent what objects based on this class can "do"
+    }
+}
---
+++ Gentle Intro to OOP/OOPBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; // contains classes such as Console and Math
+
+namespace OOPBasics
+{
+    public class Program
+    {
+        public static void Main(string[] args) // entry point....
+        {
+            // Call a static method on a class
+            string userName;
+            userName = Program.Recap("Gentle Intro to C# Basics");
+            // Create an instance of the Program class
+            Program app; // declare the object variable
+            app = new Program(); // creates the object instance
+            app.Driver(userName); // Run the Driver 
+        }
+
+        private void Driver(string name) // non-static / instance
+        {
+            Console.WriteLine();
+            Console.WriteLine("Today's topic is OOP Basics");
+            // Demonstrate a Person class
+        }
+
+        private static string Recap(string topic) // static
+        {
+            Console.WriteLine(topic);
+            // Prompt for user name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, and welcome to the C# OOP Basics!");
+            // send back the kind of information my method says it returns
+            return name;
+        }
+    }
+}
