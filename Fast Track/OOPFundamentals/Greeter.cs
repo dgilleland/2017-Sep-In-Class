ad0bce8a57c9e9bde22c316e39c88dc30a710d95
Dan Gilleland <dgilleland@nait.ca>
2017-09-18 11:49:19 -0600

    Greeter example

---
+++ Fast Track/OOPFundamentals/Greeter.cs
@@ -0,0 +1,29 @@
+﻿namespace OOPFundamentals
+{
+    public class Greeter
+    {
+        // Fields (to hold information)
+        private string _WelcomeMessage;
+        private string _FarewellMessage;
+
+        // Constructor (to set up the data for Greeter objects)
+        public Greeter(string welcomeMsg, string farewellMsg)
+        {
+            // Just store the incoming data in my fields...
+            _WelcomeMessage = welcomeMsg;
+            _FarewellMessage = farewellMsg;
+        } // end of constructor
+
+        // Methods (to allow Greeters to "speak")
+        public string SayHello()
+        {
+            return _WelcomeMessage;
+        } // end of SayHello() method
+
+        public string SayGoodbye()
+        {
+            return _FarewellMessage;
+        } // enf of SayGoodbye() method
+
+    } // end of the class definition
+} // end of namespace
---
+++ Fast Track/OOPFundamentals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace OOPFundamentals
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Let's make a more "sophisticated" Hello World program
+            // Declare an object variable based on the Greeter data type.
+            Greeter walmartGreeter;
+            // Create (instantiate) the Greeter object
+            walmartGreeter = new Greeter("Welcome to Walmart!", "Thank you for shopping at Walmart!");
+
+            Greeter klingonCommander;
+            klingonCommander = new Greeter("nuqneH!", "Qapla'!");
+
+            // Have each Greeter object speak....
+            Program.Speak(walmartGreeter);
+            Program.Speak(klingonCommander);
+        } // end of Main() method
+
+        public static void Speak(Greeter person)
+        {
+            // With this object, I can call my methods to
+            // say hello or goodbye
+            Console.WriteLine(person.SayHello());
+            Console.WriteLine(person.SayGoodbye());
+        } // end of Speak() method
+    } // end of Program class definition
+} // end of namespace
