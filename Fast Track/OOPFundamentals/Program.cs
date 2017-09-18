fcf7b16605aedea5d018d3a5b15bc932c1ac419b
Dan Gilleland <dgilleland@users.noreply.github.com>
2017-09-18 09:49:35 -0600

    Finish FastTrack Greeter

--- Fast Track/OOPFundamentals/Program.cs
+++ Fast Track/OOPFundamentals/Program.cs
@@ -10,42 +10,6 @@ namespace OOPFundamentals
     {
         public static void Main(string[] args)
         {
-            // This Main method is the entry point
-            // for this program. As such, it's the
-            // one method that we really must be
-            // making "static".
-            // By declaring it as "static", we are
-            // saying that this method can be called
-            // directly by the operating system as it
-            // starts up our program.
-
-            // A method contains a "set of instructions"
-            // that will run.
-
-            // You can write single-line comments in your code
-            // by starting the comment with a "double-slash"
-            // just like these statement here.
-            // Comments don't "run" - they are there just to
-            // provide extra information to the programmer
-            // reading this code.
-
-            // Declaring a variable inside a method will give
-            // me a "place" to hold information
-            int myAge = 32; // Variable Initialization Statement
-            int averageClassAge; // Variable declaration statement
-
-            // Assignment statement
-            averageClassAge = 23; // This stores the value 23
-
-            // Display the values on the screen
-            Console.WriteLine(myAge);
-            Console.WriteLine(averageClassAge);
-
-            // To run this program, I can press [Ctrl] + [F5]
-
-            // Our traditional "Hello World"
-            Console.WriteLine("Hello World"); // displayed on screen
-
             // Let's try creating a more "sophisticated"
             // Hello World program by using the Greeter class.
 
@@ -53,12 +17,21 @@ namespace OOPFundamentals
             Greeter walmartGreeter;
             // Create the Greeter object
             walmartGreeter = new Greeter("Welcome to Walmart!", "Have a nice day!");
+
+            Greeter klingonCommander;
+            klingonCommander = new Greeter("nuqneH!", "Qapla'!");
+
+            // Have each Greeter object speak...
+            Program.Speak(walmartGreeter);
+            Program.Speak(klingonCommander);
+        } // end of Main() 
+
+        public static void Speak(Greeter person)
+        {
             // With this object, I can call my methods to
             // say hello or goodbye
-            Console.WriteLine(walmartGreeter.SayHello());
-            Console.WriteLine(walmartGreeter.SayGoodbye());
-
-
-        }
-    }
-}
+            Console.WriteLine(person.SayHello());
+            Console.WriteLine(person.SayGoodbye());
+        } // end of Speak()
+    } // end of Program class
+} // end of namespace
