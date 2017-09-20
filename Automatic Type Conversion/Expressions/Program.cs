a86e6397f353f5b4a6cec75da31182deafc1e585
Dan Gilleland <dgilleland@users.noreply.github.com>
2017-09-20 09:35:17 -0600

    Final examples

--- Automatic Type Conversion/Expressions/Program.cs
+++ Automatic Type Conversion/Expressions/Program.cs
@@ -21,12 +21,33 @@ namespace Expressions
             Console.WriteLine(10.0 + 15.0 / 2 + 4.3);
 
             //  4)  3.0 * 4 / 6 + 6
+            Console.Write("#4)  3.0 * 4 / 6 + 6 = ");
+            Console.WriteLine(3.0 * 4 / 6 + 6);
+
             //  5)  3.0 * (4 % 6) + 6
+            Console.Write("#5)  3.0 * (4 % 6) + 6 = ");
+            Console.WriteLine(3.0 * (4 % 6) + 6);
+
             //  6)  3 * 4.0 / 6 + 6
+            Console.Write("#6)  3 * 4.0 / 6 + 6 = ");
+            Console.WriteLine(3 * 4.0 / 6 + 6);
+
             //  7)  20.0 - 2 / 6 + 3
+            Console.Write("#7)  20.0 - 2 / 6 + 3 = ");
+            Console.WriteLine(20.0 - 2 / 6 + 3);
+
             //  8)  10 + 17 % 3 + 4
+            Console.Write("#8)  10 + 17 % 3 + 4 = ");
+            Console.WriteLine(10 + 17 % 3 + 4);
+
             //  9)  (10 + 17) % 3 +4.0
+            Console.Write("#9)  (10 + 17) % 3 +4.0 = ");
+            Console.WriteLine((10 + 17) % 3 + 4.0);
+
             // 10)  10 + 17 / 4.0 + 4
+            Console.Write("#10)  10 + 17 / 4.0 + 4 = ");
+            Console.WriteLine(10 + 17 / 4.0 + 4);
+
         }
     }
 }
