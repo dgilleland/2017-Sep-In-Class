1d3fba515f4728247e7b5d40816cb89068f54796
Dan Gilleland <dgilleland@nait.ca>
2017-09-27 14:49:27 -0600

    Switch example

--- If-Else/Demos/Program.cs
+++ If-Else/Demos/Program.cs
@@ -6,14 +6,59 @@ namespace Demos
     {
         public static void Main(string[] args)
         {
-            HighestOfTwo();
+            //HighestOfTwo();
             //HighestOfThree();
             //LetterGrades();
+            LetterGradeToRange();
         } // end of Main() method
+
+        private static void LetterGradeToRange()
+        {
+            Console.Write("Enter a letter grade: ");
+            string letterGrade = Console.ReadLine();
+
+            switch (letterGrade.ToUpper()) // exact matches ONLY
+            {
+                case "A":
+                    Console.WriteLine("This is from 85 - 100");
+                    break;
+                case "B":
+                    Console.WriteLine("This is from 70 - 84");
+                    break;
+                case "C":
+                    Console.WriteLine("This is from 55 - 69");
+                    break;
+                case "D":
+                    Console.WriteLine("This is from 40 - 54");
+                    break;
+                default:
+                    Console.WriteLine("This is from 0 - 39");
+                    break;
+            }
+        }
 
         private static void LetterGrades()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("\n\nLetter Grades");
+            Console.WriteLine("-------------");
+
+            Console.Write("Enter a percentage grade (whole number): ");
+            int grade = int.Parse(Console.ReadLine());
+
+            // Find the correct range for the letter grade
+            char letterGrade;
+            if (grade >= 85)
+                letterGrade = 'A'; // 85 - 100
+            else if (grade >= 70)
+                letterGrade = 'B'; // 70 - 84
+            else if (grade >= 55)
+                letterGrade = 'C'; // 55 - 69
+            else if (grade >= 40)
+                letterGrade = 'D'; // 40 - 54
+            else
+                letterGrade = 'F'; // 0 - 39
+
+            Console.WriteLine($"The grade is a(n) '{letterGrade}'");
         }
 
         private static void HighestOfThree()
