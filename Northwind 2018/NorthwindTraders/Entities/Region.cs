2beabec9e733c2464d62ed30f8f1716a19cad294
Dan Gilleland <dgilleland@nait.ca>
2017-10-03 12:34:19 -0600

    Quick copy/past of previous demo's entity classes

---
+++ Northwind 2018/NorthwindTraders/Entities/Region.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTraders.Entities
+{
+    [Table("Region")]
+    public class Region
+    {
+        #region ColumnMappings
+        [Key]
+        public int RegionID { get; set; }
+        public string RegionDescription { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        // TODO: Region Navigation Properties
+        #endregion
+    }
+}
---
+++ Northwind 2018/NorthwindTraders/Entities/Territory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTraders.Entities
+{
+    [Table("Territories")]
+    public class Territory
+    {
+        #region Column Mappings
+        [Key]
+        public string TerritoryID { get; set; }
+        public string TerritoryDescription { get; set; }
+        public int RegionID { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        // DEMO: Many-to-Many Relationships
+        public virtual ICollection<Employee> Employees { get; set; }
+        #endregion
+    }
+}
