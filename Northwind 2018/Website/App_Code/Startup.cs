b91677e0c7e0c67b19ecfbf9a41d933ef50a7424
Dan Gilleland <dgilleland@nait.ca>
2017-10-03 12:08:42 -0600

    New Northwind 2018 Solution
    For use with the newest database, **Northwind_DMIT2018**

---
+++ Northwind 2018/Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website.Startup))]
+namespace Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
