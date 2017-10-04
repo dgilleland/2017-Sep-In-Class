ec188a62a209660116aa6e7f92b27f09c39d19d1
Dan Gilleland <dgilleland@users.noreply.github.com>
2017-10-04 08:51:27 -0600

    Looping - User Input Validation

--- Looping Structures/DemoLoops/Program.cs
+++ Looping Structures/DemoLoops/Program.cs
@@ -29,6 +29,8 @@ namespace DemoLoops
 
         private static void ProcessMenuChoice(string menuChoice)
         {
+            // Clear the screen on each menu choice
+            Console.Clear();
             switch (menuChoice)
             {
                 case "A": // Do-While Statement
@@ -42,6 +44,20 @@ namespace DemoLoops
                     break;
                 case "D": // For-Each Statement
                     ForEachDemo();
+                    break;
+                case "E": // Validate Numeric Input
+                    ValidateNumericInput();
+                    break;
+                case "F": // General user Input Validation
+                    GeneralUserValidation();
+                    break;
+                case "G": // Fibonnacci Sequences
+                    break;
+                case "H": // Perfect Numbers
+                    break;
+                case "I": // Factorials
+                    break;
+                case "J": // Prime Numbers
                     break;
                 default:
                     Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -59,6 +75,12 @@ namespace DemoLoops
             Console.WriteLine("B) While Statement");
             Console.WriteLine("C) For Statement");
             Console.WriteLine("D) For-Each Statement");
+            Console.WriteLine("E) Validate Numeric Input");
+            Console.WriteLine("F) General User Input Validation");
+            Console.WriteLine("G) Fibonnacci Sequences");
+            Console.WriteLine("H) Perfect Numbers");
+            Console.WriteLine("I) Factorials");
+            Console.WriteLine("J) Prime Numbers");
             Console.WriteLine("X) eXit");
             Console.ResetColor(); // reset colors to defaults
             Console.Write("Select an option from the menu: ");
@@ -138,6 +160,67 @@ namespace DemoLoops
             foreach (char character in text)
                 Console.WriteLine($"\t'{character}'");
         } // end of ForEachDemo() method
+
+        private static void ValidateNumericInput()
+        {
+            string userInput;
+            // Getting integers
+            int wholeNumber;
+            Console.Write("Enter a whole number: ");
+            userInput = Console.ReadLine(); // gets TEXT input
+            while (!int.TryParse(userInput, out wholeNumber))
+              // \                       \  indirect   //
+              //  \                       \  return   //
+              //   \                       \  value  //
+              //    \                       =========/
+              //     \  .TryParse() returns a bool  /
+            {
+                Console.Write("Try again. Enter a whole number: ");
+                userInput = Console.ReadLine();
+            }
+            Console.WriteLine("Thanks! You entered " + wholeNumber);
+
+            // Enter a real number
+            double realNumber;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            do
+                Console.Write("Enter a real number: ");
+            while (!double.TryParse(Console.ReadLine(), out realNumber));
+            Console.ResetColor();
+            Console.WriteLine("Great! you entered " + realNumber);
+
+        } // end of ValidateNumericInput() method
+
+        private static void GeneralUserValidation()
+        {
+            // Allow only specific text input
+            string userInput;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            do
+            {
+                Console.Write("Do you want to continue (Y/N)");
+                userInput = Console.ReadLine();
+            } while (userInput != "Y" && userInput != "N");
+            Console.ResetColor();
+            if (userInput.Equals("Y"))
+                Console.WriteLine("Great! Let's continue...");
+            else
+                Console.WriteLine("Meh. I'll continue anyway...");
+            Console.WriteLine(); // blank line...
+
+            // Validate numeric input and an acceptable range
+            int mark;
+            do
+                Console.Write("Enter a mark (0-100): ");
+            while (!int.TryParse(Console.ReadLine(), out mark)
+                   || mark < 0 || mark > 100);
+
+            Console.WriteLine($"Ok. You get a {mark}% for the course. :)");
+
+            // TODO: Write the code to ask the user for a price.
+            //       Keep asking if the price is less than $25
+            //       and greater than $50.
+        }
         #endregion
     }
 }
