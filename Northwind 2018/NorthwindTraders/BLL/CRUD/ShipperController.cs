81c349f81e12d3af7b31d945f29abf031765ab93
Dan Gilleland <dgilleland@nait.ca>
2017-10-17 07:13:49 -0600

    Practice CRUD class

---
+++ Northwind 2018/NorthwindTraders/BLL/CRUD/ShipperController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NorthwindTraders.BLL.CRUD
+{
+    public class ShipperController
+    {
+        // TODO: Complete this class as a CRUD controller for Shippers
+    }
+}
