b03db9e2077b17db9d463d4da08c832e87c0ad9b
Dan Gilleland <dgilleland@nait.ca>
2017-10-26 13:24:07 -0600

    ApplicationUser properties for being a customer or employee

--- Northwind 2018/NorthwindTraders.Security/Entities/ApplicationUser.cs
+++ Northwind 2018/NorthwindTraders.Security/Entities/ApplicationUser.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace Website
+namespace Website // TODO: replace with namespace NorthwindTraders.Security.Entities
 {
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        // Track the user's favorite color
-        public string FavoriteColor { get; set; }
+        // An application user, in this website, can be either an Employee or a Customer
+        public int? EmployeeId { get; set; }
+        public string CustomerId { get; set; }
     }
 }
-//namespace NorthwindTraders.Security.Entities
-//{
-//    class ApplicationUser
-//    {
-//    }
-//}
