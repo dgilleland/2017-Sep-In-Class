6d192b7704bf8a334227d67e26a546211cf51439
Dan Gilleland <dgilleland@nait.ca>
2017-11-02 13:13:47 -0600

    BLL CRUD methods for Shippers

--- Northwind 2018/NorthwindTraders/BLL/CRUD/ShipperController.cs
+++ Northwind 2018/NorthwindTraders/BLL/CRUD/ShipperController.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using NorthwindTraders.DAL;
+using NorthwindTraders.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace NorthwindTraders.BLL.CRUD
 {
+    [DataObject]
     public class ShipperController
     {
-        // TODO: Complete this class as a CRUD controller for Shippers
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Shipper> ListAllShippers()
+        {
+            using (var context = new NorthwindContext())
+            {
+                return context.Shippers.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Insert)]
+        public void AddShipper(Shipper info)
+        {
+            using (var context = new NorthwindContext())
+            {
+                context.Shippers.Add(info);
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update)]
+        public void UpdateShipper(Shipper info)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var existing = context.Entry(info); // Associate the info passed in with the db
+                existing.State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete)]
+        public void RemoveShipper(Shipper info)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var existing = context.Shippers.Find(info.ShipperID);
+                context.Shippers.Remove(existing);
+                context.SaveChanges();
+            }
+        }
     }
 }
