e9a0e7d5614e34b2545c4239e154607d9c4b631b
Dan Gilleland <dgilleland@nait.ca>
2017-11-06 11:02:45 -0700

    Yhatzee old school demo

---
+++ Yhatzee Old School/Yhatzee/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yhatzee
+{
+    class Program
+    {
+        // Need a random number generator
+        static Random rnd = new Random(); // (pst. this is an object.... ;)
+
+        static void Main(string[] args)
+        {
+            int[] dice = new int[5];
+
+            // "Roll" the die
+            for (int index = 0; index < dice.Length; index++)
+                dice[index] = rnd.Next(1, 7); // random number between 1 and 6
+
+            // Show the die rolls.....
+            string dieValues = string.Join(", ", dice);
+            Console.WriteLine($"Rolled the following:\n\t{dieValues}");
+
+            Reroll(dice);
+        }
+
+        static void Reroll(int[] dice)
+        {
+            Console.WriteLine("Enter an index of which die to change (zero-based, -1 to stop");
+            int index;
+            do
+            {
+                Console.Write("Die index: ");
+                while (!int.TryParse(Console.ReadLine(), out index) || index >= dice.Length)
+                    Console.Write("\tTry again: ");
+                // reroll the specified die
+                if (index >= 0)
+                    dice[index] = rnd.Next(1, 7);
+            } while (index >= 0);
+            Console.WriteLine($"New die: {string.Join(", ", dice)}");
+        }
+    }
+}
