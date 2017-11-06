50628ac2a342bac0424e1efba49535ad48fe1ebd
Dan Gilleland <dgilleland@nait.ca>
2017-11-06 10:45:57 -0700

    Search array for multiple results

--- Array Demos/Demos/Program.cs
+++ Array Demos/Demos/Program.cs
@@ -44,7 +44,51 @@ namespace Demos
                 Console.WriteLine($"We found {found} at position {foundPosition}");
             else
                 Console.WriteLine("Name was not found");
+
             // B) Searching an array for non-unique values (possible duplicates)
+            Console.WriteLine();
+            Console.Write("Enter the length of the name you want: ");
+            int searchLength;
+            while (!int.TryParse(Console.ReadLine(), out searchLength))
+                Console.Write("\tTry again: ");
+            // Allow for a possible 100% match...
+            string[] searchResults = new string[uniqueNames.Length];
+            int logicalSize = 0; // Logical size will tell us a) how many we've found
+                                 // and b) the position of the next available element
+                                 // in the searchResults.
+            for(int index = 0; index < uniqueNames.Length; index++)
+            {
+                if(uniqueNames[index].Length == searchLength)
+                {
+                    searchResults[logicalSize] = uniqueNames[index];
+                    // increment my logical size
+                    logicalSize++;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Found the following names: ");
+            for (int index = 0; index < logicalSize; index++)
+                Console.Write(searchResults[index] + ", ");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            // B-2) We can "shring" the results by copying to another array
+            string[] temp = new string[logicalSize];
+            for (int index = 0; index < logicalSize; index++)
+                temp[index] = searchResults[index];
+            // replaced the original search results with the temp
+            searchResults = temp;
+
+            Console.WriteLine("\nWe can join the string array elements into a single string, with a delimiter.");
+            Console.Write("Enter a delimiter: ");
+            string delimiter = Console.ReadLine();
+
+            // Use the string.Join() method to create a string from an array
+            string results = string.Join(delimiter, searchResults);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(results);
+            Console.ResetColor();
         }
 
         static void DemoArrayAsReferenceType()
