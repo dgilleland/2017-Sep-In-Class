0114800c64b8687cc0cc7ad2f879a18386241ba4
Dan Gilleland <dgilleland@users.noreply.github.com>
2017-11-15 09:55:54 -0700

    Finish Person demo

--- Back to OOP/OOPDemos/Person.cs
+++ Back to OOP/OOPDemos/Person.cs
@@ -12,6 +12,29 @@ namespace OOPDemos
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; private set; }
+        public string Initials
+        {
+            get
+            {
+                return $"{FirstName[0]}.{LastName[0]}.";
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                int result = 0;
+                // What should the calculation be??
+                var yearDiff = DateTime.Today.Year - BirthDate.Year;
+                var monthDiff = DateTime.Today.Month - BirthDate.Month;
+                var dayDiff = DateTime.Today.Day - BirthDate.Day;
+                if (monthDiff > 0 || (monthDiff == 0 && dayDiff >=0))
+                    result = yearDiff;
+                else
+                    result = yearDiff - 1;
+                return result;
+            }
+        }
 
         // Constructor
         public Person(string first, string last, DateTime dob)
@@ -22,6 +45,10 @@ namespace OOPDemos
             BirthDate = dob;
         }
 
-
+        // Methods
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 }
--- Back to OOP/OOPDemos/Program.cs
+++ Back to OOP/OOPDemos/Program.cs
@@ -10,6 +10,44 @@ namespace OOPDemos
     {
         static void Main(string[] args)
         {
+            Program app = new Program();
+            // directly call demos as-needed
+            app.DemoPerson();
         }
+
+        #region Demos
+        private void DemoPerson()
+        {
+            // Show how the Person class can be used
+            // Declare my variables
+            Person fred, wilma;
+            // Create (instantiate) my objects
+            fred = new Person("Fred", "Flintstone", new DateTime(105, 6, 30));
+            wilma = new Person("Wilma", "Slaghoople", new DateTime(107, 2, 24));
+            // They meet in college
+            IntroduceSelf(fred);
+            IntroduceSelf(wilma);
+
+            // Try outputting the objects themselves
+            Console.WriteLine(fred);
+            Console.WriteLine(wilma);
+
+            MarryThem(fred, wilma);
+        }
+
+        private void MarryThem(Person husband, Person wife)
+        {
+            // Traditional marriage in the cartoon series
+            wife.LastName = husband.LastName;
+            Console.WriteLine($"I now pronounce you husband and wife!");
+            Console.WriteLine($"Let me introduce Mr. {husband} and Mrs. {wife}.");
+        }
+
+        private void IntroduceSelf(Person someone)
+        {
+            string message = $"Hi. I'm {someone.FirstName}.";
+            Console.WriteLine(message);
+        }
+        #endregion
     }
 }
