8882bb75b24e6c49734428a983f6de940d4892a6
Dan Gilleland <dgilleland@nait.ca>
2017-11-15 13:53:50 -0700

    Back to OOP - Calculator

---
+++ Back to OOP/DemoOOP/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP
+{
+    public class Calculator
+    {
+        public static int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ Back to OOP/DemoOOP/Specs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP
+{
+    // All of the specification tests for the Topic.E.Example classes
+    public class Calculator_Specs
+        : Topic.E.Examples.Specs.E1_Calculator
+    {
+        protected override string AssemblyName
+        { get { return GetType().Assembly.FullName; } }
+        protected override string FullyQualifiedClassName
+        { get { return typeof(Calculator).FullName; } }
+    }
+}
