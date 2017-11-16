7c8cb4d2d1c3a4e450edb0be3cdaa4d5440bf370
Dan Gilleland <dgilleland@nait.ca>
2017-11-16 16:03:51 -0700

    Complete Person class demo

--- Back to OOP/DemoOOP/Person.cs
+++ Back to OOP/DemoOOP/Person.cs
@@ -12,6 +12,29 @@ namespace DemoOOP
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; private set; }
+        public string Initials
+        {
+            get // We can calculate the value
+            {
+                return $"{FirstName[0]}.{LastName[0]}.";
+            }
+            // No setter is needed
+        }
+        public int Age
+        {
+            get
+            {
+                int years;
+                years = DateTime.Today.Year - BirthDate.Year;
+                // See if they haven't reached their
+                // birthday for this year yet...
+                if (DateTime.Today.Month - BirthDate.Month < 0 ||
+                    (DateTime.Today.Month - BirthDate.Month == 0 &&
+                     DateTime.Today.Day - BirthDate.Day < 0))
+                    years--; // Take off a year
+                return years;
+            }
+        }
 
         // Constructor
         public Person(string first, string last, DateTime dob)
