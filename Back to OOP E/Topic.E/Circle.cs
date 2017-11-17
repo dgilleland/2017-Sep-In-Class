409c6be7438b8f038330c26cd76a070b1ab104b7
Dan Gilleland <dgilleland@nait.ca>
2017-11-17 08:36:19 -0700

    Back to OOP - Topic E

---
+++ Back to OOP E/Topic.E/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic.E
+{
+    public class Circle
+    {
+        public double Diameter { get; set; }
+        public double Radius
+        { get { return Diameter / 2; } }
+        public double Area
+        { get { return Math.PI * Radius * Radius; } }
+        public double Circumference
+        { get { return 2 * Math.PI * Radius; } }
+
+        public Circle(double d)
+        {
+            // the job of a constructor is to 
+            // ensure that all the fields/properties
+            // have "meaningful" values
+            Diameter = d;
+        }
+    }
+}
---
+++ Back to OOP E/Topic.E/Specs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Topic.E.Examples.Specs;
+
+namespace Topic.E
+{
+    public class Circle_Specs : E6_Circle
+    {
+        protected override string AssemblyName
+            => GetType().Assembly.FullName;
+        //  => is the "lambda operator"
+        // gives us a "short-hand" way of expressing
+        // our property's Getter method
+        protected override string FullyQualifiedClassName
+            => typeof(Circle).FullName;
+    }
+}
