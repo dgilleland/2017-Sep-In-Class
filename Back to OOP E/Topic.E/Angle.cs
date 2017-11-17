eb8125db34eb8102cfbfcd59bafeec03fab07bc6
Dan Gilleland <dgilleland@nait.ca>
2017-11-17 09:28:13 -0700

    Back to OOP - topic E

---
+++ Back to OOP E/Topic.E/Angle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic.E
+{
+    public class Angle
+    {
+        public double Degrees { get; set; }
+        public double Radians
+        { get { return Degrees * (Math.PI / 180); } }
+        public double Grads
+        { get { return Radians * (200 / Math.PI); } }
+
+        public Angle(double degrees)
+        {
+            Degrees = degrees;
+        }
+
+        public override string ToString()
+        {
+            return $"{Degrees}{'\u00B0'}";
+        }
+    }
+}
--- Back to OOP E/Topic.E/Program.cs
+++ Back to OOP E/Topic.E/Program.cs
@@ -10,6 +10,14 @@ namespace Topic.E
     {
         static void Main(string[] args)
         {
+            DemoAngle();
+        }
+
+        private static void DemoAngle()
+        {
+            Angle right = new Angle(90);
+            Console.WriteLine(right);
+            Console.WriteLine($"A {right} angle in Grads is {right.Grads} Grads");
         }
     }
 }
