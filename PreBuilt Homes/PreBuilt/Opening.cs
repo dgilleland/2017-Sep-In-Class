642c078fe7a46876dbd21e560b694adc76779272
Dan Gilleland <dgilleland@nait.ca>
2017-11-20 10:31:36 -0700

    PreBuilt Homes

---
+++ PreBuilt Homes/PreBuilt/Opening.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreBuilt
+{
+    public class Opening
+    {
+        // Properties for storing information
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+        public bool ExtendsToFloor { get; private set; }
+
+        // Properties for calculating information
+        public double Area { get { return Height * Width; } }
+        public double CasingLength
+        {
+            get
+            {
+                double length = Height * 2 + Width * 2;
+                if (ExtendsToFloor)
+                    length -= Width;
+                return length;
+            }
+        }
+        public double BaseBoardOffset
+        {
+            get
+            {
+                double length = 0;
+                if (ExtendsToFloor)
+                    length = Width;
+                return length;
+            }
+        }
+
+        // Constructor
+        public Opening(double height, double width, bool extendsToFloor)
+        {
+            // Store this info in the properties
+            Height = height;
+            Width = width;
+            ExtendsToFloor = extendsToFloor;
+        }
+    }
+}
---
+++ PreBuilt Homes/PreBuilt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreBuilt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opening window, door, closet;
+            // I could get the data from the user, but....
+            var window = new Opening(2, 3, false);
+            var door = new Opening(5, 2, true);
+            var closet = new Opening(5, 4, true);
+            //var age = 23;
+
+            double totalAreaToSubtract = window.Area + door.Area + closet.Area;
+            double casingToBuy = window.CasingLength + door.CasingLength + closet.CasingLength;
+            double baseboardLengthToSubtract = window.BaseBoardOffset + door.BaseBoardOffset + closet.BaseBoardOffset;
+
+
+        }
+    }
+}
