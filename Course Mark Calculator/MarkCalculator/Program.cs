aced02e48b6f77febae7c59573945b1af80cee37
Dan Gilleland <dgilleland@nait.ca>
2017-11-20 11:51:17 -0700

    Course Mark Calculator

---
+++ Course Mark Calculator/MarkCalculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Purpose - to model the idea of a course that has a number
+            //           of weighted items that are evaluated as a group
+
+            SandBox();
+        }
+
+        static void SandBox()
+        {
+            WeightedMark[] quizzes = new WeightedMark[3];
+            int index = 0;
+            while(index < quizzes.Length)
+            {
+                try
+                {
+                    // Get a quiz name/weight from the user
+                    Console.Write("Enter a quiz name: ");
+                    string name = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Enter a weight: ");
+                    double quizWeight = double.Parse(Console.ReadLine());
+
+                    // Store that in the array
+                    quizzes[index] = new WeightedMark(name, quizWeight);
+
+                    index++;
+                }
+                catch(FormatException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Unable to create a number from your input.");
+                }
+                catch(ArgumentNullException ex) // More specific
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"Error with {ex.ParamName} - {ex.Message}");
+                }
+                catch(ArgumentException ex) // less specific
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error - {ex.Message}");
+                }
+                finally
+                {
+                    Console.ResetColor();
+                }
+            }
+
+            double total = 0;
+            foreach (WeightedMark item in quizzes)
+                total += item.Weight;
+            Console.WriteLine($"There are {quizzes.Length} quizzes that total {total}%.");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine();
+            foreach (var item in quizzes)
+                Console.WriteLine($"\t{item.Weight} % - {item.Name}");
+
+            // Enter some marks
+            foreach (var quiz in quizzes)
+            {
+                Console.Write($"Enter a mark for {quiz.Name} (or leave blank): ");
+                double mark;
+                if (double.TryParse(Console.ReadLine(), out mark))
+                    quiz.EarnedMark = mark;
+            }
+            double earned = 0;
+            foreach (var item in quizzes)
+                if (item.EarnedMark.HasValue)
+                    earned += item.EarnedMark.Value;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"You have earned {earned}/{total} on your quizzes.");
+        }
+    }
+}
---
+++ Course Mark Calculator/MarkCalculator/WeightedMark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkCalculator
+{
+    public class WeightedMark
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        // adding a ? to a value type allows it to store a null value
+        public double? _EarnedMark; // a field as the "backing store"
+        public double? EarnedMark // Explicitly implement the get/set
+        {
+            get
+            {
+                return _EarnedMark;
+            }
+            set
+            {
+                if (value < 0 || value > Weight)
+                    throw new ArgumentException($"Earned marks must be from zero to {Weight}");
+                _EarnedMark = value;
+            }
+        }
+
+        public WeightedMark(string name, double weight)
+        {
+            // Validate the info coming in
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name), "Weighted marks must have a name");
+            if (name.Length > 10)
+                throw new ArgumentException("Cannot store names longer than 10 characters");
+            if (weight <= 0 || weight > 100)
+                throw new ArgumentException("Weighted marks must be greater than zero and less than or equal to 100");
+
+            // Store the input
+            Name = name.Trim(); // "Sanitize" by removing leading/trailing spaces
+            Weight = weight;
+        }
+    }
+}
