6fa286f93f71fb16aded236faebdd9c8f21c305a
Dan Gilleland <dgilleland@nait.ca>
2017-12-06 15:14:37 -0700

    PathFinder demo

---
+++ PathFinder/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; // Makes life easier
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ente a path: ");
+            string folder = Console.ReadLine();
+            string filePath = Path.Combine(folder, "ReadMe.txt");
+            var allLines = File.ReadAllLines(filePath);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (string line in allLines)
+                Console.WriteLine(line);
+            Console.ResetColor();
+        }
+    }
+}
